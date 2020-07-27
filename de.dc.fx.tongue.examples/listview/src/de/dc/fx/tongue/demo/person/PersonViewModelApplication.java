e53290133e381789b73142d80b5fe4dd59c1d5b2
cqdsp <cqdsp2202@mail.com>
2020-07-27 10:25:43 +0200

    Enhanced the listview with example data

--- de.dc.fx.tongue.examples/listview/src/de/dc/fx/tongue/demo/person/PersonViewModelApplication.java
+++ de.dc.fx.tongue.examples/listview/src/de/dc/fx/tongue/demo/person/PersonViewModelApplication.java
@@ -1,7 +1,10 @@
 package de.dc.fx.tongue.demo.person;
 
-import de.dc.fx.tongue.demo.person.controller.*;
+import de.dc.fx.tongue.demo.person.controller.PersonViewController;
+import de.dc.fx.tongue.demo.person.model.Person;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -15,8 +18,22 @@ public class PersonViewModelApplication extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		PersonViewController controller = new PersonViewController();
+		ObservableList<Person> input = FXCollections.observableArrayList();
+		createPerson(input, "Peter", "Parker");
+		createPerson(input, "Tony", "Stark");
+		createPerson(input, "Steve", "Rogers");
+		createPerson(input, "Bruce", "Wayne");
+		createPerson(input, "Clark", "Kent");
+		controller.setMasterDataPerson(input);
 		primaryStage.setScene(new Scene((Parent) controller.getRoot(), 1200, 800));
 		primaryStage.show();
+	}
+
+	private void createPerson(ObservableList<Person> input, String name, String familyname) {
+		Person p = new Person();
+		p.setName(name);
+		p.setFamilyname(familyname);
+		input.add(p);
 	}
 }
 
