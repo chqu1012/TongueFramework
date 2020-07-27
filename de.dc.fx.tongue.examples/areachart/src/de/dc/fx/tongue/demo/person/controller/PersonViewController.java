7d26be7fb2a7403c5d46fe96d21d2a064909fd0a
cqdsp <cqdsp2202@mail.com>
2020-07-27 14:00:59 +0200

    Added area chart example

---
+++ de.dc.fx.tongue.examples/areachart/src/de/dc/fx/tongue/demo/person/controller/PersonViewController.java
@@ -0,0 +1,45 @@
+package de.dc.fx.tongue.demo.person.controller;
+	
+import java.util.Random;
+
+import de.dc.fx.tongue.FXSeries;
+import de.dc.fx.tongue.FXTongueFactory;
+import de.dc.fx.tongue.FXXYData;
+import de.dc.fx.tongue.demo.person.model.PersonViewControllerIDs;
+import de.dc.fx.tonque.core.TongueControlRenderer;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+public class PersonViewController extends BasePersonViewController{
+
+	private Random r = new Random();
+	
+	@Override
+	public void initializeDatabinding() {
+		super.initializeDatabinding();
+		VBox.setVgrow(chartArea, Priority.ALWAYS);
+	}
+	
+	public void onButtonAddSeries(MouseEvent e) {
+		if (renderer instanceof TongueControlRenderer) {
+			TongueControlRenderer controlRenderer = (TongueControlRenderer) renderer;
+			int seriesCount = Integer.parseInt(model.getTextCountOfSeries());
+			for (int j = 0; j < seriesCount; j++) {
+				FXSeries fxSeries = FXTongueFactory.eINSTANCE.createFXSeries();
+				fxSeries.setName("Test "+chartArea.getData().size());
+				for (int i = 0; i < 50; i++) {
+					FXXYData data = FXTongueFactory.eINSTANCE.createFXXYData();
+					data.setX(String.valueOf(i));
+					data.setY(String.valueOf(r.nextInt()));
+					fxSeries.getInput().add(data);
+				}
+				
+				controlRenderer.addSerieById(PersonViewControllerIDs.chartArea, fxSeries );
+			}
+		}
+	}
+	
+	public void onButtonClearChart(MouseEvent e) {
+		chartArea.getData().clear();
+	}
+}
---
+++ de.dc.fx.tongue.examples/areachart/src/de/dc/fx/tongue/demo/person/model/PersonViewControllerIDs.java
@@ -0,0 +1,19 @@
+package de.dc.fx.tongue.demo.person.model;
+
+public class PersonViewControllerIDs {
+	
+	// Control ids
+	public static final String paneRoot = "paneRoot";
+	public static final String hboxChart = "hboxChart";
+	public static final String textCountOfSeries = "textCountOfSeries";
+	public static final String buttonAddSeries = "buttonAddSeries";
+	public static final String buttonClearChart = "buttonClearChart";
+	public static final String chartArea = "chartArea";
+	
+	// Validator ids
+	
+	private PersonViewControllerIDs() {
+		// IDs collection class
+	}
+}
+
